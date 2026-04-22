a071893245ad8df75f3c082a985c4bafdaf8af41
shaiksahil733 <shaiksahil73370@gmail.com>
2026-04-22 22:13:07 +0530

    deleted extra terraform configuration files

--- Terraform/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  cidr_block      = "0.0.0.0/0"
-  subnets_public  = length(var.subnets_public)
-  subnets_private = length(var.subnets_private)
-}
--- Terraform/vpc.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.vpc_main.cidr_block
-  tags = {
-    Name = "${var.network_name}-vpc"
-  }
-}
