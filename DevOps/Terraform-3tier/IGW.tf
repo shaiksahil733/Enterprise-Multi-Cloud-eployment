dab44fadabb02d61ac6969978322371fef23dd2a
shaiksahil733 <shaiksahil73370@gmail.com>
2026-04-29 15:52:49 +0530

    fixed file structure

--- DevOps/Terraform-3tier/IGW.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "main_igw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.network_name}-igw"
-  }
-}
--- phase-1/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  cidr_block      = "0.0.0.0/0"
-  subnets_private = length(var.ntier_vpc.subnets_private)
-  subnets_public  = length(var.ntier_vpc.subnets_public)
-}
